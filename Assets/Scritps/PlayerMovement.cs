49bcd3bdfbba027d1a901af005ec1faae04cab56
JohnMadrid <johnmadridcarvajal@gmail.com>
2021-04-09 15:57:46 +0200

    Player script "PlayerMoves"
    player moves with camera and camera avoids walls with help of "Cinemachine Collider" option on the bottom left of Camera view object.

--- Assets/Scritps/PlayerMovement.cs
+++ Assets/Scritps/PlayerMovement.cs
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
     // player movement
     void PlayerMoves()
     {
-        // read player inputs x and y axis
+        // read player inputs on both x and y axis
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
@@ -45,12 +45,15 @@ public class PlayerMovement : MonoBehaviour
         // player and camera move together
         if (direction.magnitude >= 0.1f)
         {
+            // Player rotates and faces the direction in which it is moving and moves where camara is pointing
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
+            
+            // smooth player rotation movement
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity,
                 _turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
+            
+            // Calculate the desired direction of movement depending on the camera movement
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * (_speed * Time.deltaTime));
         }
