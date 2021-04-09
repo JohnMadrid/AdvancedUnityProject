f613300266852b09b9e0c32ed1b1126a6dfec56c
Yesid C <yesidcanoc@gmail.com>
2021-04-10 00:31:34 +0200

    Enemy collider
    Player does not fly around

--- Assets/Scritps/PlayerMovement.cs
+++ Assets/Scritps/PlayerMovement.cs
@@ -11,41 +11,42 @@ public class PlayerMovement : MonoBehaviour
 
     //how much fore to add to the ejection of the projectile. 
     [SerializeField] private float thrust = 70f;
-    
+
     //projectile prefab
     [SerializeField] private GameObject projectilePrefab;
+
     //projectile ammunition speed
-    [SerializeField]private float projectileSpeed = 10f;
+    [SerializeField] private float projectileSpeed = 10f;
+
     // motor that drives the player
     public CharacterController controller;
     public Transform cam;
-    
-    [SerializeField]
-    private float _speed = 6f;
+
+    [SerializeField] private float _speed = 6f;
 
     private float _turnSmoothTime = 0.1f;
 
     private float _turnSmoothVelocity;
     // public
-   
+
     void Start()
     {
         // if  lives == 0 
         // // reset player position
         // transform.position = new Vector3(0f,0f,0f)
         //returns player camera
-       
-        
+
+
     }
- 
+
     // Update is called once per frame
     void Update()
 
     {
-       
+
         PlayerMoves();
     }
-    
+
     // player movement
     void PlayerMoves()
     {
@@ -53,32 +54,33 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-        
+
         // player and camera move together
         if (direction.magnitude >= 0.1f)
         {
             // Player rotates and faces the direction in which it is moving and moves where camara is pointing
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            
+
             // smooth player rotation movement
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity,
                 _turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            
+
             // Calculate the desired direction of movement depending on the camera movement
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-        
+
 
             controller.Move(moveDirection.normalized * (_speed * Time.deltaTime));
 
         }
+
         //shoot with left button mouse
         if (Input.GetMouseButtonDown(0))
         {
             //firing the projectile
             fireProjectile();
-        
+
         }
     }
 
@@ -88,13 +90,14 @@ public class PlayerMovement : MonoBehaviour
         //GameObject bullet = Instantiate(projectilePrefab, transform.position + new Vector3(0f,0.7f,0f));
         GameObject bullet = Instantiate(projectilePrefab) as GameObject;
         //places the bullet in player position.
-        bullet.transform.position = this.transform.position;
+        bullet.transform.position = this.transform.position + new Vector3(0f, 0.4f, 0f);
         bullet.transform.rotation = this.transform.rotation;
         // aplies a force, in the direction of the player, to the bullet rigidbody (Unity API)
-        //bullet.GetComponent<Rigidbody>().AddForce(this.transform.forward * 20f);
-        bullet.GetComponent<Rigidbody>().AddForce(this.transform.position * thrust);
+        bullet.GetComponent<Rigidbody>().AddForce(this.transform.forward * 20f);
+        //bullet.GetComponent<Rigidbody>().AddForce(this.transform.position * thrust);
 
     }
 }
+
 
 // TODO: decrease Player lives, create player damage
