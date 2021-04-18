3b5e9020abc9906b3b997e6a3ee5448736c55ec6
Yesid C <yesidcanoc@gmail.com>
2021-04-18 18:28:00 +0200

    Spawning rates  random

--- Assets/Scritps/SpawnManager.cs
+++ Assets/Scritps/SpawnManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    // we want to make this false when the Player is destroyed so that the spawning of the virus stops. 
+    private bool _spawningEnemy1ON = true;
     private IEnumerator coroutine;
     // Enemy1 spawns with an interval ranging from 3 to 15 seconds
     //private float _timeForNextEnemy1 = Random.Range(3.0f, 15.0f);
@@ -18,11 +20,12 @@ public class SpawnManager : MonoBehaviour
     
     private IEnumerator waitAndSpawnEnemy1()
     {
-        while (true)
+        while (_spawningEnemy1ON)
 
         {
             yield return new WaitForSeconds(Random.Range(3.0f, 15.0f));
-            GameObject enemy1Clone = Instantiate(enemy1Prefab) as GameObject;
+            //GameObject enemy1Clone = Instantiate(enemy1Prefab) as GameObject;
+            Instantiate(enemy1Prefab);
 
         }
     }
